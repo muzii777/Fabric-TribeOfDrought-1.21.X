453d90e345b9089d54bf851b9167f65ca8fab067
Muzii777 <humanny0720@gmail.com>
2025-03-22 12:20:32 +0900

    Thinking?

--- src/main/java/com/muzii/trbeofdrought/TribeOfDrought.java
+++ src/main/java/com/muzii/trbeofdrought/TribeOfDrought.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//I never think.
 public class TribeOfDrought implements ModInitializer {
 	public static final String MOD_ID = "tribe-of-drought";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
